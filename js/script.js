0e710ba76dc606f910c0594c849835192bf7ed77
“EGORBRUH” <evasilenko.ev@gmail.com>
2023-01-09 00:23:14 +0300

    js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const product = 'coffee_machine';
+const number = 5;
+const category = 'technics';
+const price = 200;
+
+
+console.log('coffee_machine')
+console.log(number * price);
